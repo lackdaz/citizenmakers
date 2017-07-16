56e74dd9f8feaa7ade0db58aff8c17f5ba588c36
seth loh <haloandwings@gmail.com>
2017-07-17 01:54:37 +0800

    updated sear

--- app/components/home/pages/activities/Sear.jsx
+++ app/components/home/pages/activities/Sear.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Example1 = () => (
   <div className="docs-example-1">
-    Example 1
+    <p>
+      You may have tried etching on wood, using laser or other forms of heat. Unless you enjoy the smell of fumes, you’d probably like to avoid that associated health hazard. You can - with plain ol’ water!
+    </p>
+    <p>
+      <h3>
+        Prerequisites
+      </h3>
+        Participants are required to be above 13 years of age, parental supervision (or both) and are required to sign an indemnity form.
+    </p>
   </div>
 );
 
