1f754db607218b4c851e10f2c8ed397e416881a9
seth loh <haloandwings@gmail.com>
2017-07-20 01:24:33 +0800

    added video

--- app/components/home/pages/intro/About.jsx
+++ app/components/home/pages/intro/About.jsx
@@ -10,12 +10,16 @@ npm start
 const Installation = () => (
   <div className="docs-installation">
     <p>
-      The citizen makers are staging another secret operation codenamed "HH Uh-oh" to find ways to synthesize common gases from water. We are not pyromaniacs (well at least, not yet). We are citizen scientists, hardware hackers, geeks just curious about what you could do with clean energy that HHO reactions that provides.
+      The citizen makers are staging a secret operation codenamed "HH Uh-oh" to find ways to synthesize common gases from water. We are not pyromaniacs (well at least, not yet). We are citizen scientists, hardware hackers, geeks just curious about what you could do with clean energy that HHO reactions that provides.
     </p>
     <p>
       In the true spirit of making, expect everyday objects, tools, controlled explosions and lots of hot glue used in the name of science and engineering!
     </p>
+    <div className="wrapper">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/7yZwoRmvi8A" frameBorder="0" allowFullScreen></iframe>
+    </div>
   </div>
+
 );
 
 export default Installation;
